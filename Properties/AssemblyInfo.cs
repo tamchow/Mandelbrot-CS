23dc5eb2db80856c5257b13e0e969ac7660af90c
Tamoghna Chowdhury <chowtam8991@gmail.com>
2017-01-29 23:53:39 +0530

    Add MIT License

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Mandelbrot")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Windows Forms Safe Fast Single-Threaded Mandelbrot Set Generator, with Smooth Coloring")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mandelbrot")]
+[assembly: AssemblyCompany("Elliott Media SDKs")]
+[assembly: AssemblyProduct("Mandelbrot Generator")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("Tamoghna Chowdhury")]
+[assembly: AssemblyTrademark("Tamoghna Chowdhury, Cameron Elliot, James Parsons")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
